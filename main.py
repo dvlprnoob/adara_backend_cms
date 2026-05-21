1c35377ad1cc26571ef5c3a2e8f40ecf178ef879
Achmad fauzi <achmadfauzi@Achmads-MacBook-Pro.local>
2026-05-21 22:02:29 +0700

    Ada di progress md

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ origins = [
     "http://localhost:5174",
     "http://127.0.0.1:5174",
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    "http://everwoodestate.com",
+    "https://everwoodestate.com",
 ]
 
 app.add_middleware(
